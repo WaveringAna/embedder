b054e3b310bcdc2d3d5c1d57f5d0584ce05d482c
Wavering Ana <ana@nekomimi.pet>
2022-11-16 05:05:26 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -64,36 +64,4 @@ router.post('/logout', function(req, res, next) {
 	});
 });
 
-router.get('/signup', function(req, res, next) {
-	res.render('signup');
-});
-
-router.post('/signup', function(req, res, next) {
-	var salt = crypto.randomBytes(16);
-	crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-		if (err) {
-			return next(err);
-		}
-		db.run('INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
-			req.body.username,
-			hashedPassword,
-			salt
-		], function(err) {
-			if (err) {
-				return next(err);
-			}
-			var user = {
-				id: this.lastID,
-				username: req.body.username
-			};
-			req.login(user, function(err) {
-				if (err) {
-					return next(err);
-				}
-				res.redirect('/');
-			});
-		});
-	});
-});
-
 module.exports = router;
